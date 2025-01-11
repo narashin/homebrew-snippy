f716d01f2f137fdc6206b603508d3d6e1e8b1a52
shinnara <me@nara.dev>
2025-01-11 13:50:23 +0900

    🔧 chore(formula): update snippy formula url
    - change url to point to the new version 1.0.0 tarball for snippy

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -1,7 +1,7 @@
 class Snippy < Formula
   desc "CLI tool for Git commit templates with emoji support"
   homepage "https://github.com/narashin/homebrew-snippy"
-  url "https://github.com/narashin/homebrew-snippy/releases/download/v1.0/snippy"
+  url "https://github.com/narashin/homebrew-snippy/releases/download/1.0.0/snippy-1.0.0.tar.gz"
   sha256 "55092d3156f683541d51e12a6d25390d1cee8e8721753b1db2dd9554f67d9c64"
   license "MIT"
 
