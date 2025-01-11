f60060c9455ae925b79b7e6dcb795c60f61a0d50
shinnara <me@nara.dev>
2025-01-11 21:10:03 +0900

    👷 ci(workflows): add GitHub Action for updating formula
    - create update-formula.yml for automation
    - automate version and sha256 updates in snippy.rb
    
    🐛 fix(formula): update sha256 checksum for snippy
    
    - change sha256 to 6384efb91442644eb21b5eb04415a7f5922b508ed1991c99705e64b0a55b098b for version integrity

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -2,7 +2,7 @@ class Snippy < Formula
   desc "CLI tool for Git commit templates with emoji support"
   homepage "https://github.com/narashin/homebrew-snippy"
   url "https://github.com/narashin/homebrew-snippy/releases/download/1.0.1/snippy-1.0.1.tar.gz"
-  sha256 "f0b606d47d628a0130a59e6f2664706a1dd8bf0d32e7cdb2eb9fc68d67ee99a8"
+  sha256 "6384efb91442644eb21b5eb04415a7f5922b508ed1991c99705e64b0a55b098b"
   license "MIT"
 
   def install
