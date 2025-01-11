9bcf2db152e7db957ba3bae3a90984c113f25a57
shinnara <me@nara.dev>
2025-01-11 21:59:28 +0900

    :rocket:

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -1,7 +1,7 @@
 class Snippy < Formula
   desc "CLI tool for Git commit templates with emoji support"
-  homepage "https://github.com/narashin/homebrew-snippy"
-  url "https://github.com/narashin/homebrew-snippy/releases/download/1.0.14/snippy-1.0.14.tar.gz"
+  homepage "https://github.com/narashin/snippy"
+  url "https://github.com/narashin/homebrew-snippy/releases/download/v1.0.14/snippy-v1.0.14.tar.gz"
   sha256 "06a45b0049885cf6c35a32178808d04da0d41e0c82405702ae404f80688376ac"
   license "MIT"
 
