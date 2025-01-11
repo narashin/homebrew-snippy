04d1a84a32676984936af9507de7f65c655bb6cd
narashin <me@nara.dev>
2025-01-11 13:09:16 +0000

    Update Snippy to version v1.0.18

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -1,8 +1,8 @@
 class Snippy < Formula
   desc "CLI tool for Git commit templates with emoji support"
   homepage "https://github.com/narashin/snippy"
-  url "https://github.com/narashin/homebrew-snippy/releases/download/v1.0.14/snippy-v1.0.14.tar.gz"
-  sha256 "06a45b0049885cf6c35a32178808d04da0d41e0c82405702ae404f80688376ac"
+  url "https://github.com/narashin/homebrew-snippy/releases/download/v1.0.18/snippy-v1.0.18.tar.gz"
+  sha256 "b9e7f21fd6a0c1ad9583b7e2b8ecf57c7eae706ab48f22e8929dc5b8be43f349"
   license "MIT"
 
   def install
