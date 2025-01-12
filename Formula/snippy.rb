a2ab801bdbb63e70c929f6dc3c23c7bbd9a50f7b
shinnara <me@nara.dev>
2025-01-12 22:15:36 +0900

    ✨ feat(formula): update snippy to version 2.0.2
    - add license field to formula
    - update url to the latest snippy package version

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -1,9 +1,10 @@
 class Snippy < Formula
   include Language::Python::Virtualenv
-
+  desc "Build Git commit messages your way"
   url "https://github.com/narashin/homebrew-snippy/releases/download/v2.0.0/snippy-2.0.0.tar.gz"
   sha256 "SHA256"
-
+  license "MIT"
+  url "https://github.com/narashin/homebrew-snippy/releases/download/v2.0.2/snippy-2.0.2-py3-none-any.whl"
   depends_on "python@3.12"
 
   def install
