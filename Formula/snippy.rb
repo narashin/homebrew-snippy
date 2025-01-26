79ed04aa970e7db539e2ab578103f3dab43eff34
narashin <me@nara.dev>
2025-01-26 03:03:07 +0000

    Update Snippy to version v2.3.0

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -1,8 +1,8 @@
 class Snippy < Formula
   desc "CLI tool to generate Git commit messages"
   homepage "https://github.com/narashin/snippy"
-  url "https://github.com/narashin/homebrew-snippy/releases/download/v2.2.1/snippy-v2.2.1.whl"
-  sha256 "e6a8f711638182194968a45f36118dbfcd98ea954ac021fff5c8f8cdca6dc78c"
+  url "https://github.com/narashin/homebrew-snippy/releases/download/v2.3.0/snippy-v2.3.0.whl"
+  sha256 "f61557c3f9de3239a66da79ebae7face5d10908e15425cd0dec637e85272feff"
   license "MIT"
 
   depends_on "python@3.12"
